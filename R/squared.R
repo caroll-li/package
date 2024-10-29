dbcba52383c212c73069c3263fe56621ec376690
caroll-li <lisijia0216@gmail.com>
2024-10-29 10:01:47 -0700

    add document

--- R/squared.R
+++ R/squared.R
@@ -1,3 +1,12 @@
+#' Squared a number or a vector
+#'
+#' @param x a number to be squared
+#'
+#' @return the square of a number
+#' @export
+#'
+#' @examples
+#' squared(3)
 squared <- function(x) {
   x^2
 }
